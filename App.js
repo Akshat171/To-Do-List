5b2d62860f7451965f8d07e7eb8778142c2095ee
Akshat171 <81281246+Akshat171@users.noreply.github.com>
2022-06-29 10:49:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+// import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
